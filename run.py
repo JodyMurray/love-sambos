7b1d273f4f5252543717e1396a9170950b53cc4f
Jody M <jodyamm@hotmail.com>
2022-07-23 07:00:58 +0000

    Add user request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -28,6 +28,7 @@ def get_sales_data():
         if validate_data(sales_data):
             print("Data is valid!")
             break
+    return sales_data
 
 
 def validate_data(values):
@@ -49,4 +50,4 @@ def validate_data(values):
     return True
 
 
-get_sales_data()
+data = get_sales_data()
